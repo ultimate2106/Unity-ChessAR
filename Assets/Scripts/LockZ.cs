c2d18645c21812763af2ccfc943dec86f6ae2209
ultimate2106 <ultimate2106@users.noreply.github.com>
2020-06-17 12:25:49 +0200

    Add LockZ Script

---
+++ Assets/Scripts/LockZ.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockZ : MonoBehaviour
+{
+    float zAxisStart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        zAxisStart = transform.localPosition.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currentX = transform.localPosition.x;
+        float currentY = transform.localPosition.y;
+        transform.localPosition = new Vector3(currentX, currentY, zAxisStart);
+    }
+}
