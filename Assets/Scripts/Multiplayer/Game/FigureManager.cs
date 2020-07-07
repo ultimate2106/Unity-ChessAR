c5cef2bf0b19fde6e0d3a2cac7879379f890b6eb
ultimate2106 <ultimate2106@users.noreply.github.com>
2020-07-07 12:17:07 +0200

    Debugging..

--- Assets/Scripts/Multiplayer/Game/FigureManager.cs
+++ Assets/Scripts/Multiplayer/Game/FigureManager.cs
@@ -5,6 +5,7 @@ public class FigureManager : MonoBehaviour
 {
     public void OnFigureMoved(Vector3 newPosition)
     {
+        Debug.Log("OnFigureMoved called..");
         PhotonView view = gameObject.GetComponent<PhotonView>();
         view.RPC("MoveFigureToNewPosition", RpcTarget.Others, newPosition);
     }
@@ -12,6 +13,7 @@ public class FigureManager : MonoBehaviour
     [PunRPC]
     public void MoveFigureToNewPosition(Vector3 newPosition)
     {
+        Debug.Log("MoveFigureToNewPosition called..");
         gameObject.transform.position = newPosition;
     }
 }
