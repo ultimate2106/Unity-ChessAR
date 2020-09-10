087e6ab0af060260606bc3bfc8efe43220777211
Dominic S <dominic.schroeder93@gmail.com>
2020-09-10 19:14:35 +0200

    Bugfix the problem with the board not showing up right after scanning the marker

---
+++ Assets/Scripts/Vuforia/CustomTrackableEventHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomTrackableEventHandler : DefaultTrackableEventHandler
+{
+    private DesignChanger _designChanger;
+
+    private void Awake()
+    {
+        _designChanger = GetComponent<DesignChanger>();
+
+        if (_designChanger == null)
+        {
+            Debug.LogError("DesignChanger is null");
+        }
+    }
+
+    protected override void OnTrackingFound()
+    {
+        _designChanger.ChangeDesign();
+        base.OnTrackingFound();
+    }
+}
